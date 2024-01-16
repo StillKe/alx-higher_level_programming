36926abbbfc72002105ca001f251d1ab958d8ee1
KijanaMgeni3 <kijanamgeni3@gmail.com>
2024-01-16 09:29:46 -0800

    A script that creates a table called first-table in the current database in the MYSQL server.id INT, name VARCHAR(256).The database name will be passed as an argument of the mysqli
    command. If te table first_table already exists, your script should not fail. SELECT and SHOW statements are not allowed

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
---Creates a table called first-table with values id name in MYSQL Server.
+--Creates a table called first_table with values id name in MYSQL Server.
 CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
